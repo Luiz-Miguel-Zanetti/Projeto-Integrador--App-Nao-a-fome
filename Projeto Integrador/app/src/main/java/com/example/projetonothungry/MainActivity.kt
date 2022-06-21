b8cc2f78e4adc6243d32817b5c67ef2cdbe3762f
Theus021 <mateusoliver2109@hotmail.com>
2022-06-21 17:10:15 -0300

    Implementação da ToolBar

--- Projeto Integrador/app/src/main/java/com/example/projetonothungry/MainActivity.kt
+++ Projeto Integrador/app/src/main/java/com/example/projetonothungry/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.projetonothungry.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,20 +21,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         setUpBottomNavigation()
 
+        //ToolBar Automatizada
+        val navHostFragment = (supportFragmentManager.findFragmentById
+            (binding.fragmentContainerView.id)) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
     }
-
 
     private fun setUpBottomNavigation() {
         with(binding.bottomNavigation){ setupWithNavController(navController) }
