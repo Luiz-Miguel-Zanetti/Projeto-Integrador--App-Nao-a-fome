0053445081613da423efbd73b57bceb3d26ce832
Raian Arnold <raian.guilherme@gmail.com>
2022-06-23 11:12:17 -0300

    Implementação do PUT

--- Projeto Integrador/app/src/main/java/com/example/projetonothungry/api/ApiService.kt
+++ Projeto Integrador/app/src/main/java/com/example/projetonothungry/api/ApiService.kt
@@ -6,16 +6,18 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ApiService {
     @GET("categoria")
     suspend fun listCategoria(): Response<List<Categoria>>
 
     @POST("produtos")
-    suspend fun addProduto(@Body produtos: Produtos) : Response<Produtos>
+    suspend fun addProduto(@Body produtos: Produtos): Response<Produtos>
 
     @GET("produtos")
-    suspend fun listProduto() : Response<List<Produtos>>
+    suspend fun listProduto(): Response<List<Produtos>>
 
-
+    @PUT("produtos")
+    suspend fun updateProduto(@Body produtos: Produtos): Response<Produtos>
 }
